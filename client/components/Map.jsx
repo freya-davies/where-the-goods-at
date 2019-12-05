d55b9e0ff03466aba7d9b937393b12338ee09fc9
Student <student@eda-bc-01.localdomain>
2019-12-05 16:27:52 +1300

    apiKey accessable on frontend

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,61 +1,60 @@
 import React, { Component } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
-
+import { getKey } from '../apis/auth'
 
 class Map extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            firstPin: { lat: -41.295910, lng: 174.773990 }
+  constructor() {
+    super()
+    this.state = {
+      firstPin: { lat: -41.295910, lng: 174.773990 },
+      key: false
+    }
+  }
+
+  componentDidMount() {
+    getKey(). then(() => {
+      this.setState({ key : true })
+    })
+  }
+
+
+  handleClick = (e) => {
+    console.log(e)
+  }
+
+
+  render() {
+    return (
+      <div>
+        {this.state.key && 
+        <LoadScript
+          id="script-loader"
+          googleMapsApiKey={process.env.GOOGLE_MAPS}>
+          <GoogleMap
+            id='Traffic-layer-example'
+            mapContainerStyle={{
+              height: "800px",
+              width: "1200px"
+            }}
+            zoom={12}
+            center={{
+              lat: -41.2743523,
+              lng: 174.735582
+            }}
+            mapTypeId='satellite'
+            onClick={this.handleClick}
+          >
+            <Marker
+              position={this.state.firstPin}
+            />
+          </GoogleMap>
+
+        </LoadScript>
         }
-    }
-
-    componentDidMount() {
-        
-    }
-
-
-    handleClick = (e) => {
-        console.log(e)
-    }
-
-
-    render() {
-        return (
-            <div>
-
-                <LoadScript
-                    id="script-loader"
-                    googleMapsApiKey={process.env.GOOGLE_MAPS}
-
-                    //paste the 2 lines below into the .env file and then REMOVE THEM FROM THIS FILE. Or we will all die.
-                    //JWT_SECRET="I Hate the Turtles! - Krang"
-                   // GOOGLE_MAPS=AIzaSyDlCzvqC9Bvt0MZ2JLsKtFQQdmRL9FmRO0
-                >
-                    <GoogleMap
-                        id='Traffic-layer-example'
-                        mapContainerStyle={{
-                            height: "800px",
-                            width: "1200px"
-                        }}
-                        zoom={12}
-                        center={{
-                            lat: -41.2743523,
-                            lng: 174.735582
-                        }}
-                        mapTypeId='satellite'
-                        onClick={this.handleClick}
-                    >
-                        <Marker
-                        position={this.state.firstPin}
-                        />
-                    </GoogleMap>
-
-                </LoadScript>
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default Map
