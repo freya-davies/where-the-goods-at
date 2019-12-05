bc9e9adbbe953fd17f9cc4d6c2c7da72e99d3178
Student <student@eda-bc-05.localdomain>
2019-12-05 15:34:21 +1300

    change home page to be seperate form login/reg

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,78 +11,29 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      showPopUp: false
     }
 
   }
 
-  // -----------------------------------
-  // pop up info to be moved onto home page
-  // -----------------------------------
-  
   componentDidMount() {
-    if (!this.props.auth.isAuthenticated) {
-      setTimeout(this.popUp, 10000);
-    }
-  }
 
-  popUp = () => {
-    this.setState({ showPopUp: true })
-    // console.log(document.getElementById('myModal').style)
-    // document.getElementById('myModal').style.display = 'block'
-    // console.log('hello')
-  }
-
-  closeModal = () => {
-    this.setState({
-      showPopUp: false
-    })
   }
 
   render() {
     return (
       <Router>
-        {this.state.showPopUp &&
-            <div className="modal" style={{display: 'block'}} id="myModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-              <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="staticBackdropLabel">Become A Forage Extraordinaire!</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.closeModal}>
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">
-                    Get access to more options and goodies!
-                    </div>
-                  <div className="modal-footer">
-                    <Link className="btn btn-primary" to="/login">Login</Link>
-                    <Link className="btn btn-primary" to="/register">Register</Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-        }
-
         <div className="container has-text-centered">
-
           <div className="hero is-small is-primary">
             <div className="hero-body has-text-centered">
               <Link to='/' className="">
                 <h1 className="title is-1">Where The Goods At</h1>
               </Link>
               <Nav />
-              <Map />
             </div>
           </div>
-
-          <div className=''>
-            {!this.props.auth.isAuthenticated &&
-              <Route exact path="/" component={Login} />
-            }
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </div>
+            <Route exact path="/" component={Map} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
 
         </div>
       </Router>
--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 
-class Map extends Component {
+class Map extends React.Component {
 
     constructor() {
         super()
@@ -12,7 +14,22 @@ class Map extends Component {
     }
 
     componentDidMount() {
-        
+        if (!this.props.auth.isAuthenticated) {
+            setTimeout(this.popUp, 10000);
+        }
+    }
+
+    popUp = () => {
+        this.setState({ showPopUp: true })
+        // console.log(document.getElementById('myModal').style)
+        // document.getElementById('myModal').style.display = 'block'
+        // console.log('hello')
+    }
+
+    closeModal = () => {
+        this.setState({
+            showPopUp: false
+        })
     }
 
 
@@ -23,15 +40,37 @@ class Map extends Component {
 
     render() {
         return (
-            <div>
+            <React.Fragment>
+                {/* pop-up-sign-in-modal */}
+                {this.state.showPopUp &&
+                    <div className="modal" style={{ display: 'block' }} id="myModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="staticBackdropLabel">Become A Forage Extraordinaire!</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.closeModal}>
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    Get access to more options and goodies!
+                            </div>
+                                <div className="modal-footer">
+                                    <Link className="btn btn-primary" to="/login">Login</Link>
+                                    <Link className="btn btn-primary" to="/register">Register</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                }
 
                 <LoadScript
                     id="script-loader"
                     googleMapsApiKey={process.env.GOOGLE_MAPS}
 
-                    //paste the 2 lines below into the .env file and then REMOVE THEM FROM THIS FILE. Or we will all die.
-                    //JWT_SECRET="I Hate the Turtles! - Krang"
-                   // GOOGLE_MAPS=AIzaSyDlCzvqC9Bvt0MZ2JLsKtFQQdmRL9FmRO0
+                //paste the 2 lines below into the .env file and then REMOVE THEM FROM THIS FILE. Or we will all die.
+                //JWT_SECRET="I Hate the Turtles! - Krang"
+                // GOOGLE_MAPS=AIzaSyDlCzvqC9Bvt0MZ2JLsKtFQQdmRL9FmRO0
                 >
                     <GoogleMap
                         id='Traffic-layer-example'
@@ -48,14 +87,20 @@ class Map extends Component {
                         onClick={this.handleClick}
                     >
                         <Marker
-                        position={this.state.firstPin}
+                            position={this.state.firstPin}
                         />
                     </GoogleMap>
 
                 </LoadScript>
-            </div>
+            </React.Fragment>
         )
     }
 }
 
-export default Map
+const mapStateToProps = ({ auth }) => {
+  return {
+    auth
+  }
+}
+
+export default connect(mapStateToProps)(Map)
