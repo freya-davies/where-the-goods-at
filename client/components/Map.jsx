93f5f7f4d49e109fb91baa163a0cc4f4fd50c086
student <student@eda-bc-03.localdomain>
2019-12-05 14:00:45 +1300

    Centered to Wellington

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -3,35 +3,41 @@ import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 
 class Map extends Component {
-  render() {
-      console.log('hello?')
-     return (
-     <div>
-        
-      <LoadScript
-        id="script-loader"
-        googleMapsApiKey="AIzaSyDlCzvqC9Bvt0MZ2JLsKtFQQdmRL9FmRO0"
-        
-        
-      >
-        <GoogleMap
-          id='example-map'
-          mapContainerStyle={{
-            height: "400px",
-            width: "800px"
-          }}
-          zoom={7}
-          center={{
-            lat: -3.745,
-            lng: -38.523
-          }}
-        />
-          
-      
-      </LoadScript>
-      </div>
-     )
-  }
+
+
+    handleClick = (e) => {
+        console.log(e)
+    }
+
+
+    render() {
+        return (
+            <div>
+
+                <LoadScript
+                    id="script-loader"
+                    googleMapsApiKey="AIzaSyDlCzvqC9Bvt0MZ2JLsKtFQQdmRL9FmRO0"
+                >
+                    <GoogleMap
+                        id='Traffic-layer-example'
+                        mapContainerStyle={{
+                            height: "800px",
+                            width: "1200px"
+                        }}
+                        zoom={12}
+                        center={{
+                            lat: -41.2743523,
+                            lng: 174.735582
+                        }}
+                        mapTypeId='satellite'
+                        onClick={this.handleClick}
+                    >
+                    </GoogleMap>
+
+                </LoadScript>
+            </div>
+        )
+    }
 }
 
 export default Map
