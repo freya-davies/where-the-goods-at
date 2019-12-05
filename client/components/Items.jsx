294ef9e27aae0a604b5259c2b9d6a521142e7906
Student <student@eda-bc-02.localdomain>
2019-12-05 15:52:21 +1300

    add an image

--- client/components/Items.jsx
+++ client/components/Items.jsx
@@ -5,23 +5,37 @@ class AddItemForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            newItem : {}
+            newItem: {},
+        
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
-    
+        this.handleImage = this.handleImage.bind(this)
+
+
     }
 
     handleChange(e) {
-       this.setState({
-           newItem : {
-               ...this.state.newItem,
-               [e.target.name] : e.target.value
-           }
-       }) 
+        this.setState({
+            newItem: {
+                ...this.state.newItem,
+                [e.target.name]: e.target.value
+            }
+
+        })
+        console.log(this.state)
     }
 
+    handleImage(e) {
+        this.setState({
+            newItem: {
+                ...this.state.newItem,
+                [e.target.name]: e.target.files[0]
+            }
+
+        })
+    }
     handleSubmit(e) {
         e.preventDefault()
         addItem(this.state.newItem)
@@ -35,26 +49,23 @@ class AddItemForm extends React.Component {
                     <h3>Add new Item</h3>
                     <label>
                         Name
-                        <input type='text' name='name' value={this.state.name} onChange={this.handleChange} />
+                        <input type='text' name='name' onChange={this.handleChange} />
                     </label>
                     <label>
                         Description
-                        <input type='text' name='description' value={this.state.description} onChange={this.handleChange} />
+                        <input type='text' name='description' onChange={this.handleChange} />
                     </label>
                     <label>
-                       Address
-                        <input type='text' name='address' value={this.state.address} onChange={this.handleChange} />
+                        Address
+                        <input type='text' name='address' onChange={this.handleChange} />
                     </label>
-                    {/* <label>
+                    <label>
                         Photo
-                        <input type='image' src='https://ak3.picdn.net/shutterstock/videos/1068433/thumb/1.jpg' />
-                    </label> */}
+                    <input type="file" name="image" accept="image/*" onChange={this.handleImage} />
+                    </label>
                     <input type="submit" value="Submit" />
                 </form>
-
             </div>
-
-
         )
     }
 }
--- migrations/20191204174313_items.js
+++ migrations/20191204174313_items.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
     table.string('name')
     table.integer('user_id')
     table.integer('category_id')
-    table.string('img_url')
+    table.string('img')
     table.boolean('public')
     table.string('description')
     table.float('long')
