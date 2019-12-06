740a822cc49fa08b8bbbbf402eac5b0c911d23fc
student <student@eda-bc-03.localdomain>
2019-12-06 13:52:42 +1300

    Refactored

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,110 +1,94 @@
 import React, { Component } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 import { getKey } from '../apis/auth'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-
 
 class Map extends React.Component {
     
-  constructor(props) {
-      super(props)
-      console.log(props)
-      this.state = {
-          center : {
-            lat: -41.2743523,
-            lng: 174.735582
-          },
-          pins: [],
-        //   pins: [
-        //       { lat: -41.295910, lng: 174.773990 },
-        //       { lat: -41.291000, lng: 174.781520 }
-        //   ],
-        key: false
-      }
-  }
+    constructor(props) {
+        super(props)
+        console.log(props)
+        this.state = {
+            center : {
+                lat: -41.2743523,
+                lng: 174.735582
+            },
+            pins: [],
+            key: false,
+            addMode: false
+        }
+    } 
 
-  componentDidMount() {
-    getKey().then(() => {
-      this.setState({ key : true })
-    })
-
-    this.setState({
-        pins: this.props.items.items.map((item) => {
-            var location = { 
-                lat: item.lat, 
-                lng: item.long 
-            }
-            return location
+    componentDidMount() {
+        getKey().then(() => {
+            this.setState({ key : true })
         })
-    })
-  }
+
+        this.setState({
+            pins: this.props.items.map((item) => {
+                var location = { 
+                    lat: item.lat, 
+                    lng: item.long 
+                }
+                return location
+            })
+        })
+    }
+
+    toggleAddMode = (e) => {
+        this.setState({
+            addMode: !this.state.addMode
+        })
+    }
 
    
-  handleClick = (e) => {
-    this.setState({
-         pins: [...this.state.pins,
-         { lat: e.latLng.lat(), lng: e.latLng.lng() }],
-         center: { lat: e.latLng.lat(), lng: e.latLng.lng()}
+    handleAddPin = (e) => {
+        if (this.state.addMode) {
+            this.setState({
+                pins: [
+                    ...this.state.pins,
+                    { lat: e.latLng.lat(), lng: e.latLng.lng() }
+                ],
+                center: { lat: e.latLng.lat(), lng: e.latLng.lng()}
 
-    })
+            })
+        }
     }
 
 
     render() {
         return (
             <div>
-        {this.state.key && this.props.items.items &&
-        <LoadScript
-          id="script-loader"
-          googleMapsApiKey={process.env.GOOGLE_MAPS}>
-          <GoogleMap
-            id='Traffic-layer-example'
-            mapContainerStyle={{
-              height: "800px",
-              width: "1200px"
-            }}
-            zoom={12}
-            center={this.state.center}
-            mapTypeId='satellite'
-            onClick={this.handleClick}
-          >
-           {this.state.pins.map((pin) => {
-             return (
-                  <Marker
-                      position={pin}
-                  />
-             ) 
-           })}
-           {/* {
-                this.props.items.items.map((item) => {
-                    console.log(item)
-                    var location = { 
-                        lat: item.lat, 
-                        lng: item.long 
-                    }
-                    console.log(location)
-                    return (
-                        <Marker
-                            position={ location }
-                        />
-                    )
-                })
-           } */}
-          </GoogleMap>
-
-        </LoadScript>
-        }
-      </div>
+                {this.state.key && this.props.items &&
+                <LoadScript
+                    id="script-loader"
+                    googleMapsApiKey={process.env.GOOGLE_MAPS}>
+                    <GoogleMap
+                        id='Traffic-layer-example'
+                        mapContainerStyle={{
+                        height: "800px",
+                        width: "1200px"
+                        }}
+                        zoom={12}
+                        center={this.state.center}
+                        mapTypeId='satellite'
+                        onClick={this.handleAddPin}
+                    >
+                    {this.state.pins.map((pin, index) => {
+                        return (
+                            <Marker
+                                key={index}
+                                position={pin}
+                            />
+                        ) 
+                    })}
+                    </GoogleMap>
+                </LoadScript>
+                }
+                <button onClick={this.toggleAddMode}>{ this.state.addMode ? "Stop Adding Pins" : "Add Pins"}</button>
+            </div>
     )
   }
 }
 
-const mapStateToProps = ({ auth, items }) => {
-    return {
-        auth,
-        items
-    }
-}
 
-export default connect(mapStateToProps)(Map)
+export default Map
