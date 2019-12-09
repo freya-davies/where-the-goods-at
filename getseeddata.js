6a799439d7d13dfd659f645644a57a7da1bd0bfe
Student <student@eda-bc-05.localdomain>
2019-12-09 17:50:49 +1300

    Update seed data, add file to get seed data easily

---
+++ getseeddata.js
@@ -0,0 +1,12 @@
+const knex = require('knex')
+const config = require('./knexfile')
+const env = process.env.NODE_ENV || 'development'
+const connection = knex(config[env])
+
+const db = connection
+
+
+db('items').select().then(items => console.log(items))
+
+// write the following into terminal to get seed data from site: 
+//  node getseeddata.js into terminal
