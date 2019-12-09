e30d3472a28f322139a603225c01dfcfb169b619
vere tindale <veretindale@gmail.com>
2019-12-09 15:36:43 +1300

    test working. the nightmare begins

--- tests/client/components/App.test.js
+++ tests/client/components/App.test.js
@@ -3,41 +3,41 @@ import { shallow } from 'enzyme'
 
 import {App} from '../../../client/components/App'
 
-test('Title renders on App', () => {
-  // Arrange
-  const expected = 'Where the goods at'
-
-  // Act
-  const wrapper = shallow(<App auth={{isAuthenticated: false}}/>)
-  const actual = wrapper.find('h1').text()
-
-  // Debug output
-  // console.log(wrapper.debug())
-
-  // Assert
-  expect(actual).toEqual(expected)
-})
-
-// test('App has some routes when not logged in', () => {
+// test('Title renders on App', () => {
 //   // Arrange
-//   const expected = 5
+//   const expected = 'Where the goods at'
 
 //   // Act
-//   const wrapper = shallow(<App auth={{isAuthenticated: false}}/>)
-//   const actual = wrapper.find('Route').length
+//   const wrapper = shallow(<App items={{items: [1,2]}} modals={{currentModal: true}} auth={{isAuthenticated: false}} fetchPublicItems={()=> {}}/>)
+//   const actual = wrapper.find('body')
+
+//   // Debug output
+//   console.log(wrapper.debug())
 
 //   // Assert
 //   expect(actual).toEqual(expected)
 // })
 
-// test('App has one less route when logged in', () => {
-//   // Arrange
-//   const expected = 4
+ test('App has some routes when not logged in', () => {
+   // Arrange
+   const expected = 5
 
-//   // Act
-//   const wrapper = shallow(<App auth={{isAuthenticated: true}}/>)
-//   const actual = wrapper.find('Route').length
+   // Act
+   const wrapper = shallow(<App items ={{items: []}}modals={{currentModal: true}} auth={{isAuthenticated: false}} fetchPublicItems={()=> {}} />)
+   const actual = wrapper.find('Route').length
 
-//   // Assert
-//   expect(actual).toEqual(expected)
-// })
+   // Assert
+   expect(actual).toEqual(expected)
+ })
+
+//  test('App has one less route when logged in', () => {
+//    // Arrange
+//    const expected = 4
+
+//    // Act
+//    const wrapper = shallow(<App auth={{isAuthenticated: true}}/>)
+//    const actual = wrapper.find('Route').length
+
+//    // Assert
+//    expect(actual).toEqual(expected)
+//  })
