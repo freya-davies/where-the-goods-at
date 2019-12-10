a3da348dd388b0d5a822a42704f95bd6e3ae4610
Student <student@eda-bc-05.localdomain>
2019-12-10 14:22:37 +1300

    Add spaces for readibility

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -51,41 +51,50 @@ class Register extends React.Component {
       <div className="registerContainer">
         <div className="col-6 col-md-4">
           <img className="img-fluid" src="/images/forage-lettuce.png" alt="Forage &amp; Find" />
+
           <form className="login-register-form" onSubmit={this.submit}>
             <h1 className='registerTitle'>Register</h1>
             {auth.errorMessage && <><h1><span className="badge badge-danger badge-lg">{auth.errorMessage}</span></h1><br></br></>}
+
             <div className="row justify-content-start">
               <div className='col-6 reg-text'>
                 <label htmlFor="user_name" >Username:
           </label>
               </div>
+
               <div className='col-6 reg'>
                 <input required className="form-control" placeholder="Username" type="text" name="user_name" onChange={this.updateDetails} />
               </div>
             </div>
+
             <div className="row justify-content-start">
               <div className='col-6 reg-text'>
                 <label htmlFor="email" >Email:
           </label>
               </div>
+
               <div className='col-6 reg'>
                 <input required className="form-control" placeholder="Email" type="email" name="email" onChange={this.updateDetails} />
               </div>
             </div>
+
             <div className="row justify-content-start">
               <div className='col-6 reg-text'>
                 <label htmlFor="first_name">First Name:
               </label>
               </div>
+
               <div className='col-6 reg'>
                 <input required className="form-control" placeholder="First Name" type="text" name="first_name" onChange={this.updateDetails} />
               </div>
             </div>
+
             <div className="row justify-content-start">
               <div className='col-6 reg-text'>
                 <label htmlFor="lastName">Last Name:
               </label>
               </div>
+
               <div className='col-6 reg'>
                 <input required className="form-control" placeholder="Last Name" type="text" name="last_name" onChange={this.updateDetails} />
               </div>
@@ -96,21 +105,24 @@ class Register extends React.Component {
                 <label htmlFor="password">Password
             </label>
               </div>
+
               <div className='col-6 reg'>
                 <input required className="form-control" placeholder="Password" type="password" name="password" onChange={this.updateDetails} />
               </div>
             </div>
+
             <div className="row justify-content-start">
               <div className='col-6 reg-text'>
                 <label htmlFor="confirm_password">Confirm Password
               </label>
               </div>
+
               <div className='col-6 reg'>
                 <input required className="form-control" placeholder="Confirm Password" type="password" name="confirm_password" onChange={this.updateDetails} />
               </div>
             </div>
-            <div className="registerButton">
 
+            <div className="registerButton">
               <input className="btn bg-main" value="Register" type="submit" />
             </div>
           </form>
